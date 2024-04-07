c4261f00fd9e18dd0bdda7ba9c2027f50d544d00
MUHAMMAD ALI HAMZA <76821369+sadbattery@users.noreply.github.com>
2024-04-07 08:16:05 +0500

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,28 +4,30 @@ def caesar_cipher(text, shift, mode):
     result = ""
     for char in text:
         if char.isalpha():
-            # Determine if character is uppercase or lowercase
+            #####  character is uppercase or lowercase
             if char.isupper():
-                # Shift uppercase letters
+                #### for uppercase letters
                 if mode == '1':
                     result += chr((ord(char) - 65 + shift) % 26 + 65)
                 elif mode == '2':
                     result += chr((ord(char) - 65 - shift) % 26 + 65)
             else:
-                # Shift lowercase letters
+                #### FOr lowercase letters
                 if mode == '1':
                     result += chr((ord(char) - 97 + shift) % 26 + 97)
                 elif mode == '2':
                     result += chr((ord(char) - 97 - shift) % 26 + 97)
         else:
-            # Keep non-alphabetic characters unchanged
+            #### non-alphabetic characters or special characters 
             result += char
     return result
 mode=0;
-# Ask user for mode (encrypt or decrypt)
+
+
 mode = input("Enter 1 for 'encrypt' or 2 for 'decrypt': ")
 
-# Ask user to input message and shift value
+
+
 text = input("Enter the message: ")
 shift = int(input("Enter the shift value: "))
 
